5a9413cfcc9609bfd7c4e4131bc3b37e1a4ad6bb
DrashtyB <dnbatavia111@gmail.com>
2020-04-21 12:44:30 +0530

    Rendered components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Grid, makeStyles } from "@material-ui/core";
+import Page1 from "./components/page-1";
+import Page2 from "./components/page-2";
+import Page3 from "./components/page-3";
+import Page4 from "./components/page-4";
 
+const useStyles = makeStyles((theme) => ({
+  app: {
+    dispaly: "flex",
+  },
+}));
 function App() {
+  const classes = useStyles();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={classes.app}>
+      <Grid container>
+        <Grid item xs={3}>
+          <Page1 />
+        </Grid>
+        <Grid item xs={3}>
+          <Page2 />
+        </Grid>
+        <Grid item xs={3}>
+          <Page3 />
+        </Grid>
+        <Grid item xs={3}>
+          <Page4 />
+        </Grid>
+      </Grid>
     </div>
   );
 }
